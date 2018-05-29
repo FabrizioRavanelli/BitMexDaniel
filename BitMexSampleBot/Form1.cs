f7adca4f9d1197fc93472b0c187ad3df83ba195c
MrPinedita <sigfridcouto@gmail.com>
2018-05-29 23:31:21 +0200

    WebClientWithTimeout added

--- BitMexSampleBot/Form1.cs
+++ BitMexSampleBot/Form1.cs
@@ -17,7 +17,7 @@ using BitMexSampleBot.tradesking;
 using log4net;
 using log4net.Repository.Hierarchy;
 using Newtonsoft.Json;
-
+using BitMexSampleBot.Util;
 
 namespace BitMexSampleBot
 {
@@ -2672,11 +2672,15 @@ namespace BitMexSampleBot
 
         private void tmrTradesKingCoinsReader_Tick(object sender, EventArgs e)
         {
-            using (WebClient wc = new WebClient())
+            using (WebClientWithTimeout wc = new WebClientWithTimeout())
             {
-                var json = wc.DownloadString(CONST_URL_TRADES_KING_JSON);
+                wc.Encoding = Encoding.UTF8;
+                wc.Headers.Add("Content-Type", "application/json");
+                wc.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                
                 try
                 {
+                    var json = wc.DownloadString(CONST_URL_TRADES_KING_JSON);
                     TradesKingCoinsRootObject rootTradesKing = JsonConvert.DeserializeObject<TradesKingCoinsRootObject>(json);
                     ITradesKingCoin coin = rootTradesKing.GetCurrentTradesKingCoin(ActiveInstrument.Symbol);
                     double OldBBMiddle = Candles.Any() && Candles[0].Open.HasValue ? Candles[0].Open.Value : 0d;
